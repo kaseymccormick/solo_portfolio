2d69d47fe024b15af4c57e8fe5993a5f64602cc3
Kasey McCormick <mccormick.kasey@gmail.com>
2015-08-20 16:08:02 -0500

    animation complete ish

--- global.js
+++ global.js
@@ -11,32 +11,32 @@ window.onload = function() {
       mediumSkills = document.querySelectorAll(".mediumskill");
       leastSkills = document.querySelectorAll(".leastskill");
       
-  TweenMax.to(barmost, 4.5, {onStart :startMostWords, width:"180px", onComplete: startMediumBar});
+  TweenMax.to(barmost, 4.5, {onStart :startMostWords, width:"160px", onComplete: startMediumBar});
  
 }
 
 function startMostWords(){
   var tl1w = new TimelineMax();
-  tl1w.staggerFrom(mostSkills, 1, { y:-100, ease:Quart.easeOut, opacity:0}, 0.5); 
+  tl1w.staggerTo(mostSkills, 1, { y:20, ease:Quart.easeOut, opacity:1}, 0.5); 
 }
 
 function startMediumBar(){
-    TweenMax.to(barmedium, 8, {onStart:startMediumWords, width:"580px", onComplete: startLeastBar });
+    TweenMax.to(barmedium, 8, {onStart:startMediumWords, width:"520px", onComplete: startLeastBar });
 }
 
 function startMediumWords(){
   var tl2w = new TimelineMax();
-  tl2w.staggerFrom(mediumSkills, 1, { y:-100, ease:Quart.easeOut, opacity:0}, 0.5);
+  tl2w.staggerTo(mediumSkills, 1, { y:20, ease:Quart.easeOut, opacity:1}, 0.5);
 }
 
 
 function startLeastBar(){
-  TweenMax.to(barleast, 5, {onStart: startLeastWords, width:"180px", onComplete: console.log("complete") });
+  TweenMax.to(barleast, 5, {onStart: startLeastWords, width:"160px", onComplete: console.log("complete") });
 }
 
 function startLeastWords(){
   var tl3w = new TimelineMax();
-  tl3w.staggerFrom(leastSkills, 1, {y:-100, ease:Quart.easeOut, opacity:0}, 0.5);
+  tl3w.staggerTo(leastSkills, 1, {y:20, ease:Quart.easeOut, opacity:1}, 0.5);
 }
 
 
